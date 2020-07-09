a8ad7ef5a7f819293d9168274c178fdef336852d
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-07-09 16:57:28 -0400

    Sign in Page has dropdown menu of users

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,10 +5,27 @@ class SignIn extends Component {
     render() {
         return (
             <div className='signin-container'>
-            <h3>Welcome to the Would You Rather App! Please sign in to continue.</h3>
+                <div className='welcome'>
+                <h2>Welcome to the Would You Rather App!</h2>
+                <h4>Please sign in to continue.</h4> 
+                </div>
+                <div className='signin-form'>
+                    <h1>Sign In</h1>
+                    <select>
+                        {this.props.users.map((id) => (
+                            <option value={id}>{id}</option>
+                        ))}
+                        <option value=''></option>
+                    </select>
+                </div>
             </div>
         )
     }
 }
 
-export default connect()(SignIn)
+function mapStateToProps(state) {
+    const { users } = state
+    return { users: Object.keys(users) };
+}
+
+export default connect(mapStateToProps)(SignIn)
