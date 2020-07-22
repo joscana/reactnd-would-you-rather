8a2dd536f1b09ded871020d22bdb71a32dd21fcf
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-07-22 11:15:12 -0400

    PollCard logs answered or unanswered

--- src/components/PollCard.js
+++ src/components/PollCard.js
@@ -3,7 +3,21 @@ import { connect } from 'react-redux';
 import { useParams } from "react-router-dom";
 
 class PollCard extends Component {
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if(this.state.value === null) {
+            alert('Please select an answer to vote!');
+            return;
+        }
+        console.log(this.state.value)
+    }
+    
     render() {
+        console.log(`Answered: ${this.props.answered}`)
         return(
             <div className='question-container'>
                 <h2>Would You Rather</h2>
@@ -12,14 +26,15 @@ class PollCard extends Component {
                         src={this.props.user.avatarURL}
                         alt="Random Avatar"
                     />
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <label>
-                            Select an option:
-                            <select value="Vote">
-                            <option value={this.props.optionOne}>{this.props.question.optionOne.text}</option>
-                            <option value={this.props.optionTwo}>{this.props.question.optionTwo.text}</option>
+                            <select onChange={this.handleChange}>
+                                <option value={null}>Select an answer</option>
+                                <option value='optionOne'>{this.props.question.optionOne.text}</option>
+                                <option value='optionTwo'>{this.props.question.optionTwo.text}</option>
                             </select>
                         </label>
+                        <input type="submit" value="Submit" />
                     </form>
                 </div>
             </div>
@@ -29,12 +44,15 @@ class PollCard extends Component {
 
 function mapStateToProps(state, ownProps) {
     const id = ownProps.match.params.id
-    const { questions, users } = state
+    const { questions, users, authedUser } = state
     const question = questions[id]
-    const user = users[question.author]
+    const user = users[authedUser]
+    const answeredKeys = Object.keys(user.answers)
+    const answered = answeredKeys.includes(id)
     return { 
         question: question, 
-        user: user
+        user: user,
+        answered: answered,
     };
 }
 
