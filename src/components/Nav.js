28ea9db1e26524a0a9437ca86972b7d3679c8fe3
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-07-27 12:48:07 -0400

    handle logged in state in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,34 +1,57 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export default function Nav (props) {
+export default function Nav(props) {
+
+    const links = (props.authedUser) ? (
+        <ul>
+            <li>
+                <NavLink to='/' exact activeClassName='active'>
+                    Home
+                    </NavLink>
+            </li>
+            <li>
+                <NavLink to='/add' activeClassName='active'>
+                    New Question
+                    </NavLink>
+            </li>
+            <li>
+                <NavLink to='/leaderboard' activeClassName='active'>
+                    Leader Board
+                    </NavLink>
+            </li>
+            <li>
+                Hello, {props.authedUser}
+            </li>
+            <li>
+                <NavLink to='/logout' activeClassName='active'>
+                    Logout
+                </NavLink>
+            </li>
+        </ul>
+    ) : (
+        <ul>
+            <li>
+                <NavLink to='/' exact activeClassName='active'>
+                    Home
+                    </NavLink>
+            </li>
+            <li>
+                <NavLink to='/add' activeClassName='active'>
+                    New Question
+                    </NavLink>
+            </li>
+            <li>
+                <NavLink to='/leaderboard' activeClassName='active'>
+                    Leader Board
+                    </NavLink>
+            </li>
+        </ul>
+    );
+
     return (
         <nav className='nav'>
-            <ul>
-                <li>
-                    <NavLink to='/' exact activeClassName='active'>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/add' activeClassName='active'>
-                        New Question
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/leaderboard' activeClassName='active'>
-                        Leader Board
-                    </NavLink>
-                </li>
-                <li>
-                    Hello, {props.authedUser}
-                </li>
-                <li>
-                    <NavLink to='/logout' activeClassName='active'>
-                        Logout
-                    </NavLink>
-                </li>
-            </ul>
+            {links}
         </nav>
     )
 }
